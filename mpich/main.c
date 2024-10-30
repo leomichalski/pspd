d5dea9bcd5ca3e1e9d127bd7029313779b50c93a
Leonardo M. Miranda <leonardomichalskim@gmail.com>
2024-10-30 13:17:07 -0300

    Consumer do MPI envia msg de volta para producer

--- mpich/main.c
+++ mpich/main.c
@@ -10,6 +10,7 @@
 // MPI_ANY_TAG
 
 int main(int argc, char *argv[]) {
+    MPI_Status status;
     const int STRING_MSG_LEN = 256;
     char string_msg[STRING_MSG_LEN];
     int number_msg;
@@ -22,17 +23,20 @@ int main(int argc, char *argv[]) {
     printf("rank: %d; size (nprocs): %d; pid: %d; ppid: %d.\n", rank, size, getpid(), getppid());
 
     if (rank == CONSUMER) {
-        MPI_Status status;
         int producer;
         for (producer = 1; producer < size; producer++) {
             MPI_Recv(string_msg, STRING_MSG_LEN, MPI_CHAR, producer, TAG, MPI_COMM_WORLD, &status);
             MPI_Recv(&number_msg, 1, MPI_INT, producer, TAG, MPI_COMM_WORLD, &status);
-            printf("Received string: \"%s\". Received number: %d.\n", string_msg, number_msg);
+            printf("CONSUMER: Received string: \"%s\". Received number: %d.\n", string_msg, number_msg);
+            sprintf(string_msg, "Hello, Producer %d!", producer);
+            MPI_Send(string_msg, STRING_MSG_LEN, MPI_CHAR, producer, TAG, MPI_COMM_WORLD);;
         }
     } else {
         number_msg = rank;
-        MPI_Send("Hello, MPI!", STRING_MSG_LEN, MPI_CHAR, CONSUMER, TAG, MPI_COMM_WORLD);;
+        MPI_Send("Hello, Consumer!", STRING_MSG_LEN, MPI_CHAR, CONSUMER, TAG, MPI_COMM_WORLD);;
         MPI_Send(&number_msg, 1, MPI_INT, CONSUMER, TAG, MPI_COMM_WORLD);;
+        MPI_Recv(string_msg, STRING_MSG_LEN, MPI_CHAR, CONSUMER, TAG, MPI_COMM_WORLD, &status);
+        printf("PRODUCER %d: Received string: \"%s\".\n", rank, string_msg);
     }
 
     MPI_Finalize();
